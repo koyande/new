25ed6e623e3a026a4dda7ed9074d7b0f5908304e
koyande <64630031+koyande@users.noreply.github.com>
2020-09-15 15:13:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "okta" {
   org_name  = "abc-dev-945392"
-  api_token = "00mkpuVyyEbqIBNsN_QhUdUV7cDD-BFJ7tzdzeJDRv"
+  api_token = "0088t-TLUsSCmnab-W9YGjPXhxucDhaWfpcvkvdZmJ"
   base_url  = "okta.com"
 }
